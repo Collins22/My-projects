16b5c6983113f7af38f4d1f8a026544e17dab097
Collins22 <nwosucollins35@gmail.com>
2024-06-05 17:48:48 +0100

    New

---
+++ Docs/docs.js
@@ -0,0 +1,2 @@
+//DOM
+//the DOM is a programming interface for web/HTML elements, it also structures that we can understand to interact with javascript, also includes tags, attributes, text nodes, etc and its represngted by a tree.
